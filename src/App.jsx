6caa674b91d517c4d7f08161f76fdcd0a193bd85
Sompol Techawattanalertkij <sompol13.th@gmail.com>
2021-03-21 16:57:18 +0700

    Adding toggle code for test

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import { AppProvider, useApp } from './contexts/app-context';
 import FirstLevel from './components/FirstLevel';
 
-const App = () => {
-  const { getToken, restoreToken } = useApp();
+// This will make all children re-render.
+// const App = () => {
+//   const { getToken, restoreToken } = useApp();
+//   return (
+//     <div>
+//       <button onClick={() => restoreToken('lCvWuxIVAi')}>Sign In</button>
+//       <h4>token: {getToken()}</h4>
+//       <FirstLevel />
+//     </div>
+//   );
+// }
 
+// This will not make all children re-render.
+const App = () => {
   return (
     <div>
-      <button onClick={() => restoreToken('lCvWuxIVAi')}>Sign In</button>
-      <h4>token: {getToken()}</h4>
+      <button>Sign In</button>
+      <h4>token: </h4>
       <FirstLevel />
     </div>
   );
